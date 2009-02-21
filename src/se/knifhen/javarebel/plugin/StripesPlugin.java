82b2d8b7842896c610f3863b866248d3f7b482ec
Andreas Knifh <knifhen@knifhen-desktop.(none)>
2009-02-21 10:11:58 +0100

    Upgraded to JavaRebelSDK 2.0.RC1

--- src/se/knifhen/javarebel/plugin/StripesPlugin.java
+++ src/se/knifhen/javarebel/plugin/StripesPlugin.java
@@ -16,9 +16,9 @@ public class StripesPlugin implements Plugin {
 	 * Adds the IntegrationProcessors.
 	 */
 	public void preinit() {
-		LoggerFactory.getInstance().echo("---------------------------------------------------------------------------------------");
-		LoggerFactory.getInstance().echo("JavaRebel: The Stripes Plugin is now enabled.");
-		LoggerFactory.getInstance().echo("---------------------------------------------------------------------------------------");
+//		LoggerFactory.getInstance().echo("---------------------------------------------------------------------------------------");
+//		LoggerFactory.getInstance().echo("JavaRebel: The Stripes Plugin is now enabled.");
+//		LoggerFactory.getInstance().echo("---------------------------------------------------------------------------------------");
 
 		IntegrationFactory.getInstance().addIntegrationProcessor("net.sourceforge.stripes.controller.NameBasedActionResolver", new NameBasedActionResolverBytecodeProcessor());
 		IntegrationFactory.getInstance().addIntegrationProcessor("net.sourceforge.stripes.controller.UrlBindingFactory", new UrlBindingFactoryBytecodeProcessor());
@@ -31,6 +31,21 @@ public class StripesPlugin implements Plugin {
 	public boolean checkDependencies(ClassLoader cl, ClassResourceSource crs) {
 		LoggerFactory.getInstance().echo("StripesPlugin.checkDependencies");
 		return false;
+	}
+
+	@Override
+	public String getDescription() {
+		return "Adds reloading of Stripes ActionBeans.";
+	}
+
+	@Override
+	public String getId() {
+		return "stripes_plugin";
+	}
+
+	@Override
+	public String getName() {
+		return "Stripes plugin 1.0.4";
 	}
 
 }
