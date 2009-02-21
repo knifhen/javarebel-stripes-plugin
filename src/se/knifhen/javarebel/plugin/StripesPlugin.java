486d7f2c908167406f4cab7cccafda4ba0ee0f77
Andreas Knifh <knifhen@knifhen-desktop.(none)>
2009-02-21 10:46:49 +0100

    implemented checkDependencies()

--- src/se/knifhen/javarebel/plugin/StripesPlugin.java
+++ src/se/knifhen/javarebel/plugin/StripesPlugin.java
@@ -3,7 +3,6 @@ package se.knifhen.javarebel.plugin;
 
 import org.zeroturnaround.javarebel.ClassResourceSource;
 import org.zeroturnaround.javarebel.IntegrationFactory;
-import org.zeroturnaround.javarebel.LoggerFactory;
 import org.zeroturnaround.javarebel.Plugin;
 
 /**
@@ -16,36 +15,33 @@ public class StripesPlugin implements Plugin {
 	 * Adds the IntegrationProcessors.
 	 */
 	public void preinit() {
-//		LoggerFactory.getInstance().echo("---------------------------------------------------------------------------------------");
-//		LoggerFactory.getInstance().echo("JavaRebel: The Stripes Plugin is now enabled.");
-//		LoggerFactory.getInstance().echo("---------------------------------------------------------------------------------------");
-
 		IntegrationFactory.getInstance().addIntegrationProcessor("net.sourceforge.stripes.controller.NameBasedActionResolver", new NameBasedActionResolverBytecodeProcessor());
 		IntegrationFactory.getInstance().addIntegrationProcessor("net.sourceforge.stripes.controller.UrlBindingFactory", new UrlBindingFactoryBytecodeProcessor());
 		IntegrationFactory.getInstance().addIntegrationProcessor("net.sourceforge.stripes.integration.spring.SpringHelper", new SpringHelperBytecodeProcessor());
 	}
 
-	/**
-	 * This method is not used by the Stripes plugin
-	 */
 	public boolean checkDependencies(ClassLoader cl, ClassResourceSource crs) {
-		LoggerFactory.getInstance().echo("StripesPlugin.checkDependencies");
-		return false;
+		return crs.getClassResource("net.sourceforge.stripes.controller.NameBasedActionResolver") != null;
 	}
 
-	@Override
 	public String getDescription() {
 		return "Adds reloading of Stripes ActionBeans.";
 	}
 
-	@Override
 	public String getId() {
 		return "stripes_plugin";
 	}
 
-	@Override
 	public String getName() {
-		return "Stripes plugin 1.0.4";
+		return "Stripes plugin 1.0.5";
+	}
+	
+	public String getAuthor() {
+		return "Andreas Knifh";
+	}
+	
+	public String getWebsite() {
+		return "http://github.com/knifhen/javarebel-stripes-plugin/tree/master";
 	}
 
 }
