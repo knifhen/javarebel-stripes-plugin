b25afaa0a6b019f8de088de677b8efc1bb5ad244
Andreas Knifh <knifhen@knifhen-desktop.(none)>
2009-03-18 21:21:31 +0100

    Version 1.0.10-SNAPSHOT

--- src/se/knifhen/javarebel/plugin/StripesPlugin.java
+++ src/se/knifhen/javarebel/plugin/StripesPlugin.java
@@ -36,7 +36,7 @@ public class StripesPlugin implements Plugin {
 	}
 
 	public String getName() {
-		return "Stripes plugin 1.0.9";
+		return "Stripes plugin 1.0.10";
 	}
 	
 	public String getAuthor() {
